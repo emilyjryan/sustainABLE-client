95ec11c0348a773162f6820ce013a3fe9e4ef891
emilyjryan <emilyryanmac@gmail.com>
2023-01-29 14:51:56 -0800

    updated NewHabit and HabitsForm

--- src/components/HabitsForm.jsx
+++ src/components/HabitsForm.jsx
@@ -10,48 +10,48 @@ export default function HabitsForm(props) {
                     type='text'
                     id='name'
                     placeholder="habit name"
-                    value={form.name}
-                    onChange={e => setForm({ ...form, name: e.target.value })}
+                    value={props.form.name}
+                    onChange={e => props.setForm({ ...props.form, name: e.target.value })}
                     />
                 <label htmlFor="description">Description</label>
                 <input  
                     type='text'
                     id='description'
                     placeholder="description"
-                    value={form.description}
-                    onChange={e => setForm({ ...form, description: e.target.value })}
+                    value={props.form.description}
+                    onChange={e => props.setForm({ ...props.form, description: e.target.value })}
                     />
                 <label htmlFor="purpose">Habit name:</label>
                 <input  
                     type='text'
                     id='purpose'
                     placeholder="purpose"
-                    value={form.purpose}
-                    onChange={e => setForm({ ...form, purpose: e.target.value })}
+                    value={props.form.purpose}
+                    onChange={e => props.setForm({ ...props.form, purpose: e.target.value })}
                     />
                 <label htmlFor="name">Impact:</label>
                 <input  
                     type='text'
                     id='impact'
                     placeholder="impact"
-                    value={form.impact}
-                    onChange={e => setForm({ ...form, impact: e.target.value })}
+                    value={props.form.impact}
+                    onChange={e => props.setForm({ ...props.form, impact: e.target.value })}
                     />
                 <label htmlFor="timeSpent">Time required:</label>
                 <input  
                     type='text'
                     id='timeSpent'
                     placeholder="time required"
-                    value={form.timeSpent}
-                    onChange={e => setForm({ ...form, timeSpent: e.target.value })}
+                    value={props.form.timeSpent}
+                    onChange={e => props.setForm({ ...props.form, timeSpent: e.target.value })}
                     />
                 <label htmlFor="imgURL">Picture URL:</label>
                 <input  
                     type='text'
                     id='imgURL'
                     placeholder="picture URL"
-                    value={form.imgURL}
-                    onChange={e => setForm({ ...form, imgURL: e.target.value })}
+                    value={props.form.imgURL}
+                    onChange={e => props.setForm({ ...props.form, imgURL: e.target.value })}
                     />
             </form>
         </div>
