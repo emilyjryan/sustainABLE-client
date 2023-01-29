a78d0c01bba3b0d391ff7b5fb63cbe88d524ced1
curtlen88 <curtlen88@gmail.com>
2023-01-29 14:21:41 -0700

    home page rendering, details page in progress not working

--- src/components/HabitsList.jsx
+++ src/components/HabitsList.jsx
@@ -1,8 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 export default function HabitsList() {
+    // set up state for habits
+    const [habits, setHabits] = useState([])
+    // const [detailID, setDetailID] = useState('')
+
+    // set up useEffect to get all habits from the habits model
+    useEffect(() => {
+        const fetchHabits = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/habits`)
+                setHabits(response.data)
+                console.log(response.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchHabits()
+    }, [])
+
+    
+
+    const listHabits = habits.map((habit => {
+        return (
+            <div key={habit._id}>
+                <h3>{habit.habit}</h3>
+                <p>{habit.description}</p>
+                <button>
+                    <a href={`/habits/${habit._id}`}>Details</a>
+                </button>
+            </div>
+        )
+    })) 
+
     return (
         <div>
-            <h1>habits List</h1>
             {/* map and list all habit from the habits model */}
+            {listHabits}
         </div>
     )
 }
