33f642c94abdfe6262bb067d31f3bb832cd703d3
emilyjryan <emilyryanmac@gmail.com>
2023-01-31 15:32:57 -0800

    fixed flexbox

--- src/components/FavoritesList.jsx
+++ src/components/FavoritesList.jsx
@@ -2,8 +2,8 @@ export default function FavoritesList(props) {
     const favslist = props.favs.favHabits.map((fav => {
         return (
             <div key={fav._id}>
-            <div className="card" style={{width: '18rem', height: '30rem'}}>
-                <img src={fav?.imgURL} alt={fav?.habit}/>
+            <div className="card" style={{width: '18rem', height: '24rem'}}>
+                <img style={{width: '18rem', height: '15rem'}} src={fav?.imgURL} alt={fav?.habit}/>
             <div className="card-body">
                 <h5 className="card-title">{fav?.habit}</h5>
                 <p className="card-text">{fav?.description}</p>
@@ -15,7 +15,7 @@ export default function FavoritesList(props) {
     }
     ))
     return (
-        <div className='d-flex flexwrap justify-content-around'>
+        <div className='d-flex flex-wrap justify-content-around '>
             {favslist}
         </div>
     )
