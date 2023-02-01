0ce10d86fb6f7e1f4e88b7c115d3dc3cbe3af0ae
cailinshaffer <cailinshaffer@gmail.com>
2023-01-31 19:57:11 -0700

    nav bar p1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,86 @@
 import { Link } from 'react-router-dom'
 
 export default function Navbar({ currentUser, handleLogout }) {
+	
 	 const loggedIn = (
 		<>
 			{/* if the user is logged in... */}
-			<Link to="/">
-				<span onClick={handleLogout}>logout</span>
-			</Link>
 
-			<Link to="/users/:id">
-				Profile
-			</Link>
-			<Link to="/habits/new">
-				Create a New Habit
-			</Link>
-
-		</>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+	    <div className="container-fluid">
+		<a className="navbar-brand" href="/">Sustain-able</a>
+		<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span className="navbar-toggler-icon"></span>
+		</button>
+		<div className="collapse navbar-collapse" id="navbarSupportedContent">
+		  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+			<li className="nav-item">
+			  <a className="nav-link active" aria-current="page" href="/">Home</a>
+			</li>
+			<li className="nav-item">
+			  <a className="nav-link active" aria-current="page" href="/users/:id">Profile</a>
+			</li>
+			<li className="nav-item">
+			  <a className="nav-link active" aria-current="page" href="/habits/new">Create a new Habit</a>
+			</li>
+			<li className="nav-item">
+			<span onClick={handleLogout}>Logout</span>
+			</li>	
+		  </ul>
+		</div>
+	  </div>
+	</nav> 
+	</>
 	 )
+
+	
 
 	 const loggedOut = (
 		<>
 			{/* if the user is not logged in... */}
-			<Link to="/users/register">
+			{/* <Link to="/users/register">
 				Register
 			</Link>{' | '}
 
 			<Link to="/users/login">
 				Login
-			</Link>
-		</>
-	 )
+			</Link> */}
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+	    <div className="container-fluid">
+		<a className="navbar-brand" href="/">Sustain-able</a>
+		<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span className="navbar-toggler-icon"></span>
+		</button>
+		<div className="collapse navbar-collapse" id="navbarSupportedContent">
+		  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+			<li className="nav-item">
+			  <a className="nav-link active" aria-current="page" href="/">Home</a>
+			</li>	
+			<li className="nav-item">
+			  <a className="nav-link active" aria-current="page" href="/users/login">Login</a>
+			</li>	
+			<li className="nav-item">
+			  <a className="nav-link active" aria-current="page" href="/users/register">Register</a>
+			</li>	
+		  </ul>
+		</div>
+	  </div>
+	</nav> 
+	</>
+	 	)
 
 	return (
-		<nav>
-			{/* user always sees this section */}
-			<Link to="/">
-				<p>Home</p>
-			</Link>
-
+		<>
 			{currentUser ? loggedIn : loggedOut}
-		</nav>
+		
+		</>
+		)
+	}
 
 
 
 
 
+	
+	
 
-
-	)
-}
